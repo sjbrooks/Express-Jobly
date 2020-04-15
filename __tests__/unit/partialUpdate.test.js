6cbdd1fc8e404297c0f104ac10bb61bcbf907c00
Jay Vigilla <JayRVigilla@gmail.com>
2020-04-15 12:25:38 -0700

    Sarah found the orphan 's' from jest errors

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -16,14 +16,14 @@ describe("partialUpdate()", () => {
     VALUES ('FicInc', 
       'PretendCo', 
       456, 
-      'pretends to fix the world's problems', 
+      'pretends to fix the world''s problems', 
       'https://res.cloudinary.com/teepublic/image/private/s--RCdOAK-K--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_jpg,h_630,q_90,w_630/v1539852739/production/designs/3341218_0.jpg')
     RETURNING handle
       `)
     }
 )
 
-  it("should generate a proper partial update query with just 1 field",
+  it("should generate a proper partial update query with just 2 fields",  //originally written test asked for 1 field
     async function () {
 
       // // FIXME: write real tests!
@@ -35,9 +35,10 @@ describe("partialUpdate()", () => {
 
       let c = await sqlForPartialUpdate('companies', items, 'handle', 'FicInc')
       let {query, values} = c;
-      console.log(query);
-      console.log(values);
+      // console.log(query);
+      // console.log(values);
       expect(query).toEqual("UPDATE companies SET name=$1, num_employees=$2 WHERE handle=$3 RETURNING name, num_employees, handle");
+      expect(values).toEqual([ 'Larry Inc', 876543, 'FicInc' ]);
       
       //attempted to test for if update happened
       // why doesn't testRes run/update?
