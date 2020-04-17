b36b8bf3ad1f1f2808f8d6e9d70f2742bb611c4f
Sarah <sjbrooks305@gmail.com>
2020-04-17 10:46:06 -0400

    reverted back to old version of buildSearchQuery

--- helpers/buildSearchQuery.js
+++ helpers/buildSearchQuery.js
@@ -6,74 +6,46 @@
  * 
  */
 
-
 function sqlForSearchQuery(search, min_employees, max_employees) {
-  let idx = 1;
   let query = 'SELECT handle, name FROM companies';
   let values = [];
   let addQuery = ' WHERE ';
-
-  function buildMinMaxQueryLine(min_employees, max_employees) {
-
-    console.log("\n\n\n\n Made it into buildminmaxqueryline")
-
-    let minMaxQueryLine;
-
-    console.log("\n\n\n\n min_employees in buildminmaxqueryline before reassignment is", min_employees)
-    min_employees = min_employees || 0;
-    console.log("\n\n\n\n min_employees in buildminmaxqueryline after reassignment is", min_employees)
-    max_employees = max_employees || 100000000;
-
-    minMaxQueryLine = (`(num_employees BETWEEN $${idx} AND $${idx + 1})`);
-    console.log("\n\n\n\n minMaxQueryLine in buildminmaxqueryline is", minMaxQueryLine)
-    return minMaxQueryLine;
-  }
 
   if (!search && !min_employees && !max_employees) {
     return { query, values };
   }
 
   if (search) {
-    addQuery += (`(lower(name) LIKE $${idx} OR lower(handle) LIKE $${idx})`);
-    idx++;
+    addQuery += ('(lower(name) LIKE $1 OR lower(handle) LIKE $1)');
+    console.log("\n\n\n\n addQuery inside of if(search) inside buildSerachQuery BEFORE nested if block is ", addQuery);
+
     values.push(`%${search.toLowerCase()}%`);
 
-    console.log("addQuery is inside search before nested if is ", addQuery)
-
     if (min_employees || max_employees) {
-      let minMaxQueryLine = buildMinMaxQueryLine(min_employees, max_employees);
-      addQuery += (' AND ' + minMaxQueryLine);
-      console.log("addQuery is inside search insde nested if is ", addQuery)
+      min_employees = min_employees || 0;
+      max_employees = max_employees || 100000000;
+
+      addQuery += (' AND (num_employees BETWEEN $2 AND $3)');
       query += addQuery;
-
       values.push(min_employees, max_employees);
-
       return { query, values }
     }
 
-    query += addQuery;
-    console.log("THE VALUES INSIDE SEARCH", values)
+    console.log("\n\n\n\n addQuery inside of if(search) inside buildSerachQuery is ", addQuery);
+    query += (addQuery);
     return { query, values }
-    
   }
-  
-  // if (search && (min_employees || max_employees)) {
-    //   query += ' AND ';
-    // }
-    
-    if (min_employees || max_employees) {
-      let minMaxQueryLine = buildMinMaxQueryLine(min_employees, max_employees);
-      addQuery += minMaxQueryLine;
-      query += addQuery;
-      
-      values.push(min_employees, max_employees);
-      
-      console.log("THE VALUES INSIDE MIN MAX", values)
+
+  if (min_employees || max_employees) {
+    min_employees = min_employees || 0;
+    max_employees = max_employees || 100000000;
+
+    addQuery += (' (num_employees BETWEEN $1 AND $2)');
+    query += (addQuery);
+    values.push(min_employees, max_employees);
     return { query, values }
   }
 }
-
-
 
 
 module.exports = sqlForSearchQuery;
